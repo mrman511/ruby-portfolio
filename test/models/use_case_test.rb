2866a7d5e542b3a094570af8ff016ec5abc0a792
Paul Bodner <bodner80@gmail.com>
2025-02-24 18:19:44 -0800

    created_use_case_model

---
+++ test/models/use_case_test.rb
@@ -0,0 +1,57 @@
+require "test_helper"
+
+class UseCaseTest < ActiveSupport::TestCase
+  setup do
+    @valid_use_case_params = {
+      name: "server",
+      icon: File.open(Rails.root.join("test", "fixtures", "files", "default-avatar.jpg"))
+    }
+  end
+
+  test "#create adds a use_case to the data base with valid params" do
+    assert_difference("UseCase.count") {
+      UseCase.create!(@valid_use_case_params)
+    }
+  end
+
+  test "#create creates a use_case that can be retrieved from the database" do
+    created_use_case = UseCase.create!(@valid_use_case_params)
+    assert_nothing_raised { UseCase.find(created_use_case.id) }
+  end
+
+  test "#name is a required attribute" do
+    assert_raises(ActiveRecord::RecordInvalid) {
+      UseCase.create!(@valid_use_case_params.except(:name))
+    }
+  end
+
+  test "#name is titleized when UseCase is created" do
+    use_case = UseCase.create!(@valid_use_case_params)
+    assert_not_equal @valid_use_case_params[:name], use_case.name
+    assert_equal @valid_use_case_params[:name].titleize, use_case.name
+  end
+
+  test "#name must be unique" do
+    UseCase.create!(@valid_use_case_params)
+    assert_raises(ActiveRecord::RecordInvalid) {
+      UseCase.create!(@valid_use_case_params)
+    }
+  end
+
+  test "#icon is a file attachment" do
+    created_use_case = UseCase.create!(@valid_use_case_params)
+    assert created_use_case.icon.attached?
+  end
+
+  test "#icon can be attached after use_case has been instantiated" do
+    created_use_case = UseCase.create!(@valid_use_case_params.except(:icon))
+    created_use_case.icon.attach(@valid_use_case_params[:icon])
+    assert created_use_case.icon.attached?
+  end
+
+  test "#icon can be removed" do
+    created_use_case = UseCase.create!(@valid_use_case_params)
+    created_use_case.icon.purge
+    assert_not created_use_case.icon.attached?
+  end
+end
