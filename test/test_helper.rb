6c0a06839e024a448a86ded6cf4c26632bb1e282
Paul Bodner <bodner80@gmail.com>
2025-03-22 22:45:10 -0700

    added custom error reporting to rails minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+require "minitest/reporters"
+require File.expand_path("support/failure_reporter", __dir__)
+
+Minitest::Reporters.use! [ FailureCollectorReporter.new ]
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
